01cc0eed28bdb1e90da388c00776b18b2e22e802
Mike Leonard <mtleonardjr@gmail.com>
2021-02-10 20:09:30 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import {
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
+      <div className='app'>
+        <nav className='navbar'>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -27,15 +27,17 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <div className='main-page' >
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </div>
         </Switch>
       </div>
     </Router>
@@ -43,7 +45,7 @@ export default function App() {
 }
 
 function Home() {
-  return <h2>Home</h2>;
+  return <h2 className='home'>Home</h2>;
 }
 
 function About() {
