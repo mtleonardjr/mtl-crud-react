ec815aba3b8cd27b0b1aa74aa3b7f9fb9a8cc56d
Mike Leonard <mtleonardjr@gmail.com>
2021-02-13 17:46:15 -0500

    styled search

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -2,6 +2,16 @@ const Columns = () => {
     return (
       <div className="row">
         <div className="column">
+          <form className="search">
+            <div className='delete' >
+            <input type='submit'></input>
+            </div>
+            <label className='search-label'>Search Label</label>
+            <input type='text'></input>
+            <input type='submit'></input>
+          </form>
+        </div>
+        <div className="column">
           <div className="column-1">
             <div className="column-item">Column 1</div>
             <div className="column-item">Column 1</div>
