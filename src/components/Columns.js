054fe2ffea6c4905b1039e98a1ec65f05b4f1334
Mike Leonard <mtleonardjr@gmail.com>
2021-02-13 18:56:34 -0500

    added styling

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -1,29 +1,45 @@
 const Columns = () => {
     return (
       <div className="row">
-        <div className="column">
-          <form className="search">
+        <div className="column-search">
+          <form className="card-search">
             <div className='delete' >
               <button>x</button>
             </div>
-            <label className='search-label'>Search Label</label>
-            <input type='text'></input>
+            <label className='card-label'>Search Label</label>
+            <input type='text' className='text-input'></input>
             <input type='submit'></input>
           </form>
         </div>
         <div className="column">
-          <form className="add-form">
+          <form className="card">
             <div className='delete' >
               <button>x</button>
             </div>
-            <label className='search-label'>Add Note</label>
-            <input type='text'></input>
-            <input type='text'></input>
+            <label className='card-label'>Add Note</label>
+            <input type='text' className='text-input'></input>
+            <input type='text' className='text-input'></input>
             <input type='submit'></input>
           </form>
         </div>
         <div className="column">
-          <div className="column-1">
+          <div className="card-records">
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
+            <div className="column-item">Column 1</div>
             <div className="column-item">Column 1</div>
             <div className="column-item">Column 1</div>
             <div className="column-item">Column 1</div>
